84012fac63d14c2ab48eb920172bffbee8d2f127
Vidhi-Ajmera <vidhi2005ajmera@gmail.com>
2026-06-01 16:37:19 +0530

    backend URL added

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import './index.css';
 import App from './App';
+
+// Set global API base URL to the deployed backend
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'https://session-psi.vercel.app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
