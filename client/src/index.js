095f4e3fc74e8a2b879ef6bcd79771d12532db2d
Vidhi-Ajmera <vidhi2005ajmera@gmail.com>
2026-06-03 19:54:45 +0530

    added be link

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 // Set global API base URL to the deployed backend
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'https://session-psi.vercel.app/';
 // Send cookies with every request (required for HTTP-only JWT auth)
 axios.defaults.withCredentials = true;
 
